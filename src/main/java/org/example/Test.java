fb2da232b69ed46d1234518d733d30cff0526e7d
smrat <smratxyt9old@gmail.com>
2024-08-24 11:44:39 +0400

    [afpl

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,20 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test {
+
+    @Test
+    public void testFactorialPositiveNumber() {
+        Assert.assertEquals(Factorial.factorial(5), 120);
+    }
+
+    @Test
+    public void testFactorialZero() {
+        Assert.assertEquals(Factorial.factorial(0), 1);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFactorialNegativeNumber() {
+        Factorial.factorial(-1);
+    }
+}
