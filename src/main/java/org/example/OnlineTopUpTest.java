a36c0a78bbe97e2bc99992ea0454c32226a73579
smrat <smratxyt9old@gmail.com>
2024-08-24 12:54:22 +0400

    Лек16 мом

--- src/main/java/org/example/OnlineTopUpTest.java
+++ src/main/java/org/example/OnlineTopUpTest.java
@@ -14,18 +14,16 @@ public class OnlineTopUpTest {
 
     @BeforeClass
     public void setUp() {
-        // Укажите путь к вашему WebDriver
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver"); // Надо указать путь к личному WebDriver, я оставлю пустым
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // Запуск браузера в фоновом режиме
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get("https://mts.by"); // URL сайта mts.by
+        driver.get("https://mts.by");
     }
 
     @Test
     public void testPlaceholderText() {
-        // Проверка надписей в незаполненных полях для каждого варианта оплаты услуг
         checkPlaceholderText("//input[@id='service-input']", "Услуги связи");
         checkPlaceholderText("//input[@id='internet-input']", "Домашний интернет");
         checkPlaceholderText("//input[@id='installment-input']", "Рассрочка");
@@ -34,11 +32,9 @@ public class OnlineTopUpTest {
 
     @Test(dependsOnMethods = "testPlaceholderText")
     public void testServicePayment() {
-        // Выбор варианта «Услуги связи»
         WebElement serviceOption = driver.findElement(By.id("service-option"));
         serviceOption.click();
 
-        // Заполнение полей
         WebElement phoneNumberField = driver.findElement(By.id("phone-number"));
         phoneNumberField.sendKeys("1234567890");
 
@@ -48,23 +44,18 @@ public class OnlineTopUpTest {
         WebElement continueButton = driver.findElement(By.id("continue-button"));
         continueButton.click();
 
-        // Проверка попапа
         WebElement popup = driver.findElement(By.id("payment-popup"));
 
-        // Проверка суммы
         WebElement displayedAmount = popup.findElement(By.id("displayed-amount"));
         Assert.assertEquals(displayedAmount.getText(), "100");
 
-        // Проверка номера телефона
         WebElement displayedPhoneNumber = popup.findElement(By.id("displayed-phone-number"));
-        Assert.assertEquals(displayedPhoneNumber.getText(), "1234567890");
+        Assert.assertEquals(displayedPhoneNumber.getText(), "297777777");
 
-        // Проверка надписей в незаполненных полях для ввода реквизитов карты
         checkPlaceholderText("//input[@id='card-number']", "Номер карты");
         checkPlaceholderText("//input[@id='expiry-date']", "Срок действия");
         checkPlaceholderText("//input[@id='cvv']", "CVV");
 
-        // Проверка наличия иконок платёжных систем
         WebElement paymentIcons = popup.findElement(By.id("payment-icons"));
         Assert.assertTrue(paymentIcons.findElements(By.className("visa")).size() > 0, "Иконка Visa отсутствует");
         Assert.assertTrue(paymentIcons.findElements(By.className("mastercard")).size() > 0, "Иконка MasterCard отсутствует");
