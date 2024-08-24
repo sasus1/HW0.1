8f570c22495240c6e80c3558938fe92603d2ef41
smrat <smratxyt9old@gmail.com>
2024-08-24 13:48:41 +0400

    73

--- src/main/java/org/example/ArrayProcessor.java
+++ src/main/java/org/example/ArrayProcessor.java
@@ -3,7 +3,7 @@ package org.example;
 public class ArrayProcessor {
     public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
         if (array.length != 4) {
-            throw new MyArraySizeException("Массив должен содержать 4 строки.");
+            throw new MyArraySizeException("Массив] должен содержать 4 строки.");
         }
 
         for (String[] row : array) {
